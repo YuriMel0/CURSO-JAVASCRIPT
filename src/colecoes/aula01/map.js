84832a8b4db6f4e5f32d1a317c41d563fa394701
Yuri Mel0 <meloy316@gmail.com>
2022-05-13 18:34:27 -0300

    adicionando exemplo da aula sobre estrutura Map no js

---
+++ src/colecoes/aula01/map.js
@@ -0,0 +1,14 @@
+// principais metodos no Map
+const myMap = new Map()
+
+myMap.set('maca', 'fruta')
+// Map(1) {"maça" => "fruta"}
+
+myMap.get(maca)
+// "fruta"
+
+myMap.delete("maca")
+// true
+
+myMap.get("maca")
+// undefined
