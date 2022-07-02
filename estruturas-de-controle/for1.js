dc4f6271ba4a4c3f8b111cd293802184dfe320fc
Yuri Mel0 <meloy316@gmail.com>
2022-07-02 12:32:44 -0300

    adicionando exemplos com for em Js

---
+++ estruturas-de-controle/for1.js
@@ -0,0 +1,8 @@
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+const notas = [6.7, 5.9, 9.1, 7.5, 7.7];
+for (let i = 0; i < notas.length; i++) {
+    console.log(`Nota = ${notas[i]}`);
+}
