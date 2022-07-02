7c8be36d2f3d48177abfd08b4adc36bfea0eae9d
Yuri Mel0 <meloy316@gmail.com>
2022-07-02 17:37:41 -0300

    adicionando mais exemplos sobre params e retorno

---
+++ funcao/paramsEretornos.js
@@ -0,0 +1,15 @@
+// Exemplo ruim mas para fins de teste
+function area(largura, altura) {
+    const area = largura * altura
+    if (area > 20) {
+        console.log(`valor acima do permitido ${area}m²`)
+    } else {
+        return area
+    }
+}
+
+console.log(area(2,2))
+console.log(area(2))
+console.log(area())
+console.log(area(2,3,4,5,6,7,8))
+console.log(area(5,5))
