aca070d840f808b3a80014952dd05929c53a8bd9
Yuri Mel0 <meloy316@gmail.com>
2022-07-02 17:24:48 -0300

    adicionando exemplos de como criar funcoes em Js

---
+++ funcao/cidadaoPrimeiraClasse.js
@@ -0,0 +1,33 @@
+// Função em Js é First-Class Object 
+// Higth-order function
+
+// Criar de forma literal
+function fun1() { }
+
+// Armazenar em uma variavel
+const fun2 = function ( ) { }
+
+// Armazenar dentro de um array
+const array = [function (a, b) { return a + b}, fun1, fun2]
+console.log(array[0](2,3))
+
+// Armazenar em um atributo de objeto
+const obj = { }
+obj.falar = function ( ) { return 'Opa!'}
+console.log(obj.falar())
+
+// Passar função como parametro para outra
+function run(fun) {
+    fun()
+}
+
+run(function () { console.log('Executando...')})
+
+// Uma função pode retornar / conter uma função
+function soma(a, b) {
+    return function (c) {
+        console.log(a + b +c)
+    }
+}
+
+soma(2,3)(4)
