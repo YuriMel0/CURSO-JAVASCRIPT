4d69d077f38afff433f055c15531b82703565c25
Yuri Mel0 <meloy316@gmail.com>
2022-07-07 15:15:04 -0300

    adicionando exemplo de conceito oo em Js

---
+++ objeto/oo.js
@@ -0,0 +1,23 @@
+// CÓDIGO NÂO EXECUTAVEL
+// Apenas exemplo
+
+// Procedural
+processamento(valor1, valor2, valor3) // exemplo
+
+// OO
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento () {
+        //. . .
+    }
+}
+
+objeto.processamento() // O foco passou a ser o objeto, antes era a função
+
+// Principios importantes:
+// 1. abstração
+// 2. encapsulamento
+// 3. herança
+// 4. polimorfismo
