fd5df47641606d414ef71d390019afa45d393030
Yuri Mel0 <meloy316@gmail.com>
2022-07-08 18:28:34 -0300

    adicionando exemplo da diferença entre JSON e Obj

---
+++ objeto/JSONvsObj.js
@@ -0,0 +1,10 @@
+// Convertendo de objeto para JSON
+const obj = { a: 1, b: 2, c: 3, soma() { return a + b + c}}
+console.log(JSON.stringify(obj))
+
+//console.log(JSON.parse("{a: 1, b: 2, b:3}"))
+//console.log(JSON.parse("{'a': 1, 'b': 2, 'c': 3}"))
+
+// Convertendo de JSON para objeto
+console.log(JSON.parse('{"a": 1, "b": 2, "c": 3}'))
+console.log(JSON.parse('{"a": 1, "b": "string", "c": true, "d": {}, "e": [] }'))
