60e7f236ec0d16c7aab6b847168340523930644e
Yuri Mel0 <meloy316@gmail.com>
2022-07-11 17:34:35 -0300

    adicionando exemplos com reduce em Js

---
+++ array/reduce1.js
@@ -0,0 +1,11 @@
+const alunos = [
+    {nome: 'João', nota: 7.3, bolsista: false},
+    {nome: 'Maria', nota: 9.2, bolsista: true},
+    {nome: 'Pedro', nota: 9.8, bolsista: false},
+    {nome: 'Ana', nota: 8.7, bolsista: true}
+]
+console.log(alunos.map(a => a.nota))
+const resultado = alunos.map(a => a.nota).reduce(function(acumulador, atual) { // [ 7.3, 9.2, 9.8, 8.7 ] ele soma os elemetos de 2 em 2
+    console.log(acumulador, atual)
+    return acumulador + atual
+})
