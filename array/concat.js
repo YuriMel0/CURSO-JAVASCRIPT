f8dff161894aa76eb246010d7bf3b3e037f272f1
Yuri Mel0 <meloy316@gmail.com>
2022-07-11 18:07:07 -0300

    adicionando exemplo de concat em Js

---
+++ array/concat.js
@@ -0,0 +1,6 @@
+const filhas = ['Valesca', 'Cibalena']
+const filhos = ['Woshington', 'Uesclei']
+const todos = filhas.concat(filhos, 'fulano')
+console.log(todos, filhas, filhos)
+
+console.log([].concat([1, 2], [3, 4], 5, [[6, 7]]))
