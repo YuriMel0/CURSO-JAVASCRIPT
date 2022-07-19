c66fefe491d3aa0195265fd77e4f2cbbb9113c1b
Yuri Mel0 <meloy316@gmail.com>
2022-07-19 18:51:55 -0300

    adicionando exemplos com map e set em Js

---
+++ esnext/map.js
@@ -0,0 +1,19 @@
+const tecnologias = new Map()
+tecnologias.set('React', { framework: false})
+tecnologias.set('Angular', { framework: true})
+console.log(tecnologias.get('React'))
+
+const chavesVariadas = new Map([
+    [function() { }, 'Função'],
+    [{}, 'Objeto'],
+    [123, 'Numero']
+])
+
+chavesVariadas.forEach((vl, ch) => {
+    console.log(ch, vl)
+})
+
+console.log(chavesVariadas.has(123))
+chavesVariadas.delete(123)
+console.log(chavesVariadas.has(123))
+console.log(chavesVariadas.size)
